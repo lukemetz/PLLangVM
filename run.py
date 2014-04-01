f92ed9ad591301eb7334ec29082a4e2041142138
Chris <sihrc.c.lee@gmail.com>
2014-04-01 11:24:39 -0400

    undoing changes to run.py

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ if os.path.exists(output + ".s"):
     os.remove(output + ".s")
 
 os.system("rlwrap sml run.sml " + sys.argv[1])
-os.system("sml run.sml " + sys.argv[1])
 print "Compiling with llvm..."
 os.system("llc " + output + ".ll")
 os.system("gcc " + output + ".s" + " -o " + output)
