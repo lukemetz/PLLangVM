20185d73cd021f6aff6def4c57e504c70452f88f
= <luke.metz@students.olin.edu>
2014-04-27 22:50:08 +0000

    added some tests

---
+++ tests.py
@@ -0,0 +1,37 @@
+import unittest
+from run import *
+from subprocess import call
+import sys
+
+def run_sml(filename):
+	cleanBuild(filename)
+	print os.path.dirname(filename)
+	os.makedirs(os.path.join("build", os.path.dirname(filename)))
+	if "win" in sys.platform:
+		compileWin(filename)
+	elif "darwin" in sys.platform:
+		compileOSX(filename)
+	elif "linux" in sys.platform:
+		compileLinux(filename)
+	else:
+		print "What are you"
+		pass
+	executable = ".".join(filename.split(".")[:-1])
+	output_file = executable+"_output"
+	call(executable, stdout=open(output_file,'wb+'))
+	result = open(output_file, "r").read().strip()
+
+	return result
+class Test(unittest.TestCase):
+	def test_basic(self):
+		self.assertEquals(run_sml("tests/basic.plg"), "2")
+	def test_if(self):
+		self.assertEquals(run_sml("tests/if.plg"), "4")
+	def test_curry(self):
+		self.assertEquals(run_sml("tests/curry.plg"), "9")
+	def test_fib(self):
+		self.assertEquals(run_sml("tests/fib.plg"), "89")
+	def test_anon(self):
+		self.assertEquals(run_sml("tests/anon.plg"), "42")
+if __name__ == "__main__":
+	unittest.main()
